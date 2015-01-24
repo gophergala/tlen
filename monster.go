3362b3c0bb1d190952017c9fc8cda4399f21ad45
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2015-01-24 23:37:55 +0700

    subactions reusable

--- monster.go
+++ monster.go
@@ -1,25 +1,13 @@
 package main
 
 import (
-	"log"
 	"strconv"
 
 	"github.com/seletskiy/go-android-rpc/android"
 	"github.com/seletskiy/go-android-rpc/android/sdk"
 )
 
-type SubActions []*SubAction
-
-type SubAction struct {
-	Title       string
-	Description string
-	PreDraw     func(subaction *SubAction) bool
-	Answers     SubActions
-}
-
-var uids []string
-
-var subactions = SubAction{
+var scenario = SubAction{
 	Title:       "",
 	Description: "Danger! First step. What do you feel?",
 	Answers: SubActions{
@@ -68,70 +56,5 @@ var subactions = SubAction{
 }
 
 func InitDreamWithMonster(action *Action, button sdk.Button) {
-	subactions.Draw()
-}
-
-type SubActionButtonHandler struct {
-	subaction *SubAction
-	button    *sdk.Button
-}
-
-func (handler SubActionButtonHandler) OnClick() {
-	if handler.subaction.PreDraw == nil || handler.subaction.PreDraw(handler.subaction) {
-		handler.subaction.Draw()
-	}
-}
-
-func (subactions *SubActions) Draw() {
-	for _, subaction := range *subactions {
-		button := createView("android.widget.Button").(sdk.Button)
-		button.SetText1s(subaction.Title)
-		android.OnClick(button, SubActionButtonHandler{
-			subaction,
-			&button,
-		})
-		attachView(button.View)
-	}
-}
-
-func (subaction *SubAction) Draw() {
-	hideAll()
-
-	desc := createView("android.widget.TextView").(sdk.TextView)
-	desc.SetText1s(subaction.Description)
-	attachView(desc.View)
-
-	subaction.Answers.Draw()
-}
-
-func hideAll() {
-	for _, uid := range uids {
-		view := android.GetViewById("main_layout", uid)
-		if textView, ok := view.(sdk.TextView); ok {
-			log.Printf("%#v\n", textView)
-			textView.SetVisibility(ViewGone)
-		} else if button, ok := view.(sdk.Button); ok {
-			log.Printf("%#v\n", button)
-			button.SetVisibility(ViewGone)
-		} else {
-			log.Printf("%#v\n", "vse govno!")
-		}
-	}
-
-	uids = []string{}
-}
-
-var viewUid = 100
-
-func createView(what string) interface{} {
-	viewUid++
-
-	uid := strconv.Itoa(viewUid)
-	uids = append(uids, uid)
-
-	return android.CreateView(uid, what)
-}
-
-func attachView(view sdk.View) {
-	android.AttachView(view, strconv.Itoa(MainLayoutId))
+	scenario.Draw()
 }
