56a6e2b33f4fd141e7ff6734c591b1ee53800a67
Alexey Baranov <a.baranov@office.ngs.ru>
2015-01-24 10:29:33 +0600

    go-android-rpc implementation started

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/seletskiy/go-android-rpc/android"
+
+	// required for linking
+	_ "github.com/seletskiy/go-android-rpc/android/modules"
+	"github.com/seletskiy/go-android-rpc/android/sdk"
+)
 
 type Scene struct {
 	Plot []string
 	Next *Scene
+}
+
+var SceneOne = &Scene{
+	Plot: []string{
+		"welcome to tlen",
+		"enjoy the ride",
+	},
+	Next: SceneTwo,
+}
+
+var SceneTwo = &Scene{
+	Plot: []string{
+		"30 years ago...",
+	},
 }
 
 func (scene *Scene) Run() {
@@ -32,23 +54,24 @@ func (scene *Scene) GetText() string {
 	return text
 }
 
+type NextTextButtonHandler struct {
+	button   sdk.Button
+	textView sdk.TextView
+}
+
+func (handler NextTextButtonHandler) OnClick() {
+	handler.button.PerformHapticFeedback(0)
+	handler.textView.SetText1s(SceneOne.GetText())
+}
+
+func start() {
+	textView := android.GetViewById("main_layout", "plot_text").(sdk.TextView)
+	nextTextButton := android.GetViewById("main_layout", "next_text").(sdk.Button)
+	android.OnClick(nextTextButton, NextTextButtonHandler{nextTextButton, textView})
+}
+
 func main() {
-	SceneOne := &Scene{
-		Plot: []string{
-			"welcome to tlen",
-			"enjoy the ride",
-		},
-	}
+	android.OnStart(start)
 
-	SceneTwo := &Scene{
-		Plot: []string{
-			"30 years ago...",
-		},
-	}
-
-	SceneOne.Next = SceneTwo
-
-	SceneOne.Run()
-
-	fmt.Println("\ngame over")
+	android.Enter()
 }
